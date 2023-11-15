8b02e57d1d7d675b622698b9f74f86fbfb0c6cee
Dado20066 <95567837+Dado20066@users.noreply.github.com>
2023-11-15 13:30:00 +0100

    Add files via upload

---
+++ WORKSPACE TPSIT/tpsit/FCFS.java
@@ -0,0 +1,38 @@
+package it.edu.iisgubbio;
+
+public class FCFS { 
+
+	public static void main(String[] args) {
+
+		String[] Processo = new String[4];
+		int[] tArrivo = new int [4];
+		int[] tTempodiEsecuzione = new int [4];
+		int[] tTempodiServizio= new int [4]; 
+		int[] tTempodiAttesa = new int [4]; 
+		double media; 
+
+
+		Processo[0] = "P0";
+		Processo[1] = "P1";
+		Processo[2] = "P2";
+		Processo[3] = "P3";
+
+		tArrivo[0] = 0;
+		tArrivo[1] = 1;
+		tArrivo[2] = 2;
+		tArrivo[3] = 3;
+
+		tTempodiEsecuzione[0] = 5;
+		tTempodiEsecuzione[1] = 3;
+		tTempodiEsecuzione[2] = 8;
+		tTempodiEsecuzione[3] = 6;
+
+		//tempo di esecuzione - tempo do arrivo
+		
+		for(int i=0;i<=0;i++) {
+			tTempodiAttesa[i]=tTempodiEsecuzione[i]-tArrivo[i];
+		}
+		System.out.println();
+
+	}
+}
