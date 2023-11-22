5e0e3b8ab783faea741f5683d0fa850f872493bb
Dado20066 <95567837+Dado20066@users.noreply.github.com>
2023-11-22 12:36:05 +0100

    Update FCFS.java

--- WORKSPACE TPSIT/tpsit/FCFS.java
+++ WORKSPACE TPSIT/tpsit/FCFS.java
@@ -1,41 +1,51 @@
 package it.edu.iisgubbio;
-
-public class FCFS { 
-
+public class FcFs {
 	public static void main(String[] args) {
-
-		String[] Processo = new String[4];
-		int[] tArrivo = new int [4];
-		int[] tTempodiEsecuzione = new int [4];
-		int[] tTempodiServizio= new int [4]; 
-		int[] tTempodiAttesa = new int [4]; 
-		double media; 
-
-
-		Processo[0] = "P0";
-		Processo[1] = "P1";
-		Processo[2] = "P2";
-		Processo[3] = "P3";
-
-		tArrivo[0] = 0;
-		tArrivo[1] = 1;
-		tArrivo[2] = 2;
-		tArrivo[3]  = 3;
-
+		
+		String [] Posprocesso = new String [4];
+		int [] arrivo = new int [4];
+		int [] tTempodiEsecuzione = new int [4];
+		int [] servizio = new int [4];
+		int [] attesa = new int [4];
+		
+		double mediaAttesa;
+		
+		Posprocesso[0] = "P0";
+		Posprocesso[1] = "P1";
+		Posprocesso[2] = "P2";
+		Posprocesso[3] = "P3";
+		
 		tTempodiEsecuzione[0] = 5;
 		tTempodiEsecuzione[1] = 3;
 		tTempodiEsecuzione[2] = 8;
 		tTempodiEsecuzione[3] = 6;
-
-		//tempo di esecuzione - tempo do arrivo
 		
-		for(int i=0;i<=0;i++) {
-			tTempodiAttesa[i]=tTempodiEsecuzione[i]-tArrivo[i];
+		for(int x = 0; x<4; x++) {
+			arrivo[x]=x;
 		}
-		for(int i=0;i<=0;i++) {
-			tTempodiServizio[i]=tTempodiServizio[-1];
+		
+		for(int y = 0; y<4; y++) {
+			if(y==0) {
+				
+				servizio[0]= 0;
+				
+				attesa[0]=0;
+				
+			}else {
+				
+				servizio[y]=servizio[y-1]+tTempodiEsecuzione[y-1];
+				
+				attesa[y] = servizio[y]-arrivo[y];
+			}
 		}
-		System.out.println();
-
+		
+		mediaAttesa =(double)(attesa[0]+attesa[1]+attesa[2]+attesa[3])/4;
+		
+		for(int i = 0; i<=3; i++) {
+			System.out.println(Posprocesso[i]);
+		}
+		
+		System.out.println("la media attesa è: "+mediaAttesa);
+		
 	}
 }
