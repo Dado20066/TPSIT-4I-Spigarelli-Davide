343f662ae34a37b3454b4ac81a988dd79d5a3be2
Dado20066 <95567837+Dado20066@users.noreply.github.com>
2023-12-12 18:22:52 +0100

    Inserito commenti roundRobin.java

--- WORKSPACE TPSIT/tpsit/roundRobin.java
+++ WORKSPACE TPSIT/tpsit/roundRobin.java
@@ -1,55 +1,59 @@
 package it.edu.iisgubbio;
 
-
 public class roundRobin {
 
-	public static void main(String[] args ) {
-		String[] processo= new String[4];
-		int []tArrivo=new int [4];
-		int []tExe=new int [4];
-		int []tServizio=new int [4];
-		double []tAttesa= new double [4];
-		double media=0;
-		int qT=3;
-		int tExeRim=0;
+    public static void main(String[] args) {
+        // Dichiarazione e inizializzazione delle variabili
+        String[] processo = new String[4];
+        int[] tArrivo = new int[4];
+        int[] tEse = new int[4];
+        int[] tServizio = new int[4];
+        double[] tAttesa = new double[4];
+        double media = 0;
+        int quantitaTempo = 3;
+        int tExeRim = 0;
 
-		processo[0]="P0";
-		processo[1]="P1";
-		processo[2]="P2";
-		processo[3]="P3";
+        // Inizializzazione dei processi
+        processo[0] = "P0";
+        processo[1] = "P1";
+        processo[2] = "P2";
+        processo[3] = "P3";
 
-		tArrivo[0]=0;
-		tArrivo[1]=1;
-		tArrivo[2]=2;
-		tArrivo[3]=3;
+        // Inizializzazione dei tempi di arrivo
+        tArrivo[0] = 0;
+        tArrivo[1] = 1;
+        tArrivo[2] = 2;
+        tArrivo[3] = 3;
 
-		tExe[0]=5;
-		tExe[1]=3;
-		tExe[2]=8;
-		tExe[3]=6;
+        // Inizializzazione dei tempi di esecuzione
+        tEse[0] = 5;
+        tEse[1] = 3;
+        tEse[2] = 8;
+        tEse[3] = 6;
 
-		System.out.println("Quanto di tempo: " + qT);
-		for(int i=0; i<4;i++) {
+        System.out.println("Quanto di tempo: " + quantitaTempo);
 
-			if(tArrivo[i]==0 ) {
-				tServizio[0]=0;
-				tAttesa[0]=0;
+        for (int i = 0; i < 4; i++) {
+            if (tArrivo[i] == 0) {
+                tServizio[0] = 0;
+                tAttesa[0] = 0;
+            } else {
+                tServizio[i] = tServizio[i - 1] + tEse[i - 1];
+                tAttesa[i] = tServizio[i] - tArrivo[i];
+                media = media + tAttesa[i];
+                tEseRim = tEse[i] - 3;
+                tEse[i] = tEseRim;
+            }
 
-			}else{
-				tServizio[i]=tServizio[i-1]+tExe[i-1];
-				tAttesa[i]=tServizio[i]-tArrivo[i];
-				media= media+tAttesa[i];
-				tExeRim=tExe[i]-3;
-				tExe[i]=tExeRim;
-			}
-			//----------------------------------------------------------------
-			System.out.println("Processo: " + processo[i]);
-			System.out.println("Tempo di arrivo: " + tArrivo[i]);
-			System.out.println("Tempo di Esecuzione: " + tExe[i]);
-			System.out.println("Tempo di servizio: "+tServizio[i]);
-			System.out.println("Tempo di attesa: "+tAttesa[i]);
-		}
-		System.out.println("Media tempi di attesa: "+media);
+            // Stampa dei dettagli del processo
+            System.out.println("Processo: " + processo[i]);
+            System.out.println("Tempo di arrivo: " + tArrivo[i]);
+            System.out.println("Tempo di Esecuzione: " + tEse[i]);
+            System.out.println("Tempo di servizio: " + tServizio[i]);
+            System.out.println("Tempo di attesa: " + tAttesa[i]);
+        }
 
-	}
+        // Calcolo e stampa della media dei tempi di attesa
+        System.out.println("Media tempi di attesa: " + media);
+    }
 }
