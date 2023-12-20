08b92f41447fd3ff7653f10a1f30e89eaea4ec22
Dado20066 <95567837+Dado20066@users.noreply.github.com>
2023-12-20 12:34:29 +0100

    Update Conta.java

--- WORKSPACE TPSIT/tpsit/threads/Conta.java
+++ WORKSPACE TPSIT/tpsit/threads/Conta.java
@@ -6,6 +6,10 @@ package it.edu.iisgubbio.threads;
  * 
  * Dato che abbiamo implementato un unica classe ch eimplementa un thread,
  * per distinguere i due hread occorre passare al costruttore una variabile di selezione
+ * 
+ * In questa verisone non abbiamo implementato uno scheduler, i thread partono 
+ * con un'ordine inpartito dalla virtual machine e stampano tutti i numeri prima di passare al secondo
+ * thread
  */
 
 public class Conta extends Thread {
@@ -15,23 +19,37 @@ public class Conta extends Thread {
 		Conta t2 = new Conta(1);
 		t1.start();
 		t2.start();
-		
-	
+
+
 	} 
 	public Conta (int type) {
 		typeThread = type;
 	}
-	
+	public void StopThread(int tempo) {
+		if(tempo==5) {
+			this.stop();
+		}
+	}
+
 	public void run() {
 		if(typeThread==0) {
 			for(int i =0; i<=10; i++) {
-			System.out.println(i);	
-			}
-			
-		}else {
-			for(int i =50; i<=100; i++) {
 				System.out.println(i);	
-				}
+				this.StopThread(i);
+
+			}
+		}
+
+
+		try {
+			this.sleep(500);	
+
+		}	catch(InterruptedException e){
+			System.out.println("errore");
+		}
+		for(int i =50; i<=100; i++) {
+			System.out.println(i);
 		}
 	}
 }
+
